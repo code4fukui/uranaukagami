13bb157bc1065ea78dd8177e5aacaf67dd029331
Taisuke Fukuno <fukuno@jig.jp>
2026-03-04 20:34:02 +0900

    update for gemma3

--- uranaukagami.js
+++ uranaukagami.js
@@ -1,9 +1,11 @@
 import { fetchWeb } from "https://code4fukui.github.io/wsutil/fetchWeb.js";
-import { fetchImageRecog } from "https://code4fukui.github.io/openai-imagerecog/fetchImageRecog.js"
+//import { fetchImageRecog } from "https://code4fukui.github.io/openai-imagerecog/fetchImageRecog.js"
+import { askImage } from "https://code4fukui.github.io/ask/askImage.js";
 
 export default fetchWeb(async (param, req, path, conninfo) => {
   if (path == "/api/imagerecog") {
-    const res = await fetchImageRecog(param.imgbin, param.prompt);
+    //const res = await fetchImageRecog(param.imgbin, param.prompt);
+    const res = await askImage(param.imgbin, param.prompt);
     return res;
   }
   return null;
